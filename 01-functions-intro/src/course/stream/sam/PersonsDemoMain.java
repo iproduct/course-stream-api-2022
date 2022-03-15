4adfd30e2b002a30cc04111dd7695f6bdce59e88
Trayan Iliev <office@iproduct.org>
2022-03-15 09:59:06 +0200

    setup SAM demo

---
+++ 01-functions-intro/src/course/stream/sam/PersonsDemoMain.java
@@ -0,0 +1,39 @@
+package course.stream.sam;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static course.stream.sam.Gender.FEMALE;
+import static course.stream.sam.Gender.MALE;
+import static course.stream.sam.PhoneKind.*;
+
+public class PersonsDemoMain {
+    public static void main(String[] args) {
+        List<Person> persons = List.of(
+                new Person(1L, "John", "Doe", 35, MALE, "jdoe@gmail.com",
+                        List.of(new Phone(WORK, "+(359) 2 987654"), new Phone(MOBILE, "+(359) 887 986543")),
+                        new Address("Sofia", "Tzar Osvoboditel 15")),
+                new Person(2L, "Jane", "Doe", 29, FEMALE, "jane.doe@gmail.com",
+                        List.of(new Phone(WORK, "+(1) 5678889989"), new Phone(MOBILE, "+(1) 889 665756")),
+                        new Address("United States","New York", "Wall Street 152")),
+                new Person(3L, "Gorge", "Harisson", 59, MALE, "georgeh@gmail.com",
+                        List.of(new Phone(HOME, "+(44) 345345464"), new Phone(MOBILE, "+(44) 445 756756")),
+                        new Address("Great Britain", "London", "Baker Steet 15")),
+                new Person(1L, "Ivan", "Petrov", 35, MALE, "jdoe@gmail.com",
+                        List.of(new Phone(WORK, "+(359) 2 345345"), new Phone(MOBILE, "+(359) 889 968686")),
+                        new Address("Sofia", "Hristo botev 54"))
+                );
+        var personsByAge = new ArrayList<>(persons);
+//        personsByAge.sort(new Comparator<Person>() {
+//            @Override
+//            public int compare(Person p1, Person p2) {
+//                return Integer.compare(p1.getAge(), p2.getAge());
+//            }
+//        });
+//        personsByAge.sort((p1, p2) -> Integer.compare(p1.getAge(), p2.getAge()));
+        personsByAge.sort(Comparator.comparingInt(Person::getAge));
+
+        personsByAge.forEach(System.out::println);
+    }
+}
---
+++ 01-functions-intro/src/course/stream/sam/Phone.java
@@ -0,0 +1,56 @@
+package course.stream.sam;
+
+public class Phone {
+    private PhoneKind kind;
+    private String number;
+
+    public Phone() {
+    }
+
+    public Phone(PhoneKind kind, String number) {
+        this.kind = kind;
+        this.number = number;
+    }
+
+    public PhoneKind getKind() {
+        return kind;
+    }
+
+    public void setKind(PhoneKind kind) {
+        this.kind = kind;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Phone)) return false;
+
+        Phone phone = (Phone) o;
+
+        if (getKind() != phone.getKind()) return false;
+        return getNumber().equals(phone.getNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getKind().hashCode();
+        result = 31 * result + getNumber().hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "kind=" + kind +
+                ", number='" + number + '\'' +
+                '}';
+    }
+}
