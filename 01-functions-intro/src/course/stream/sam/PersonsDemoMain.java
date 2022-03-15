bdfd80f0c1aa66d1171c4b56d9cb4ca93959a9f7
Trayan Iliev <office@iproduct.org>
2022-03-15 10:07:11 +0200

    setup SAM demo update

--- 01-functions-intro/src/course/stream/sam/PersonsDemoMain.java
+++ 01-functions-intro/src/course/stream/sam/PersonsDemoMain.java
@@ -12,7 +12,7 @@ public class PersonsDemoMain {
     public static void main(String[] args) {
         List<Person> persons = List.of(
                 new Person(1L, "John", "Doe", 35, MALE, "jdoe@gmail.com",
-                        List.of(new Phone(WORK, "+(359) 2 987654"), new Phone(MOBILE, "+(359) 887 986543")),
+                        List.of(new Phone(WORK, "+(1) 435345345"), new Phone(WORK, "+(359) 2 987654"), new Phone(MOBILE, "+(359) 887 986543")),
                         new Address("Sofia", "Tzar Osvoboditel 15")),
                 new Person(2L, "Jane", "Doe", 29, FEMALE, "jane.doe@gmail.com",
                         List.of(new Phone(WORK, "+(1) 5678889989"), new Phone(MOBILE, "+(1) 889 665756")),
@@ -20,7 +20,7 @@ public class PersonsDemoMain {
                 new Person(3L, "Gorge", "Harisson", 59, MALE, "georgeh@gmail.com",
                         List.of(new Phone(HOME, "+(44) 345345464"), new Phone(MOBILE, "+(44) 445 756756")),
                         new Address("Great Britain", "London", "Baker Steet 15")),
-                new Person(1L, "Ivan", "Petrov", 35, MALE, "jdoe@gmail.com",
+                new Person(1L, "Ivan", "Petrov", 39, MALE, "jdoe@gmail.com",
                         List.of(new Phone(WORK, "+(359) 2 345345"), new Phone(MOBILE, "+(359) 889 968686")),
                         new Address("Sofia", "Hristo botev 54"))
                 );
