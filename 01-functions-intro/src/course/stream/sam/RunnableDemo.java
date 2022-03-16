9a18f2028364c15c2934328ca3c09f34c2121f7a
Trayan Iliev <office@iproduct.org>
2022-03-16 10:03:06 +0200

    RunnableDemo demo added

--- 01-functions-intro/src/course/stream/sam/RunnableDemo.java
+++ 01-functions-intro/src/course/stream/sam/RunnableDemo.java
@@ -1,5 +1,12 @@
 package course.stream.sam;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 class RocketLiftOff implements Runnable {
     private int n;
 
@@ -22,9 +29,24 @@ class RocketLiftOff implements Runnable {
 }
 
 public class RunnableDemo {
+    public static BiConsumer<Integer, ExecutorService> rocketStart = (Integer n, ExecutorService executor) -> { // Closure
+        executor.execute(() -> {
+            for (int i = 10; i > 0; i--) {
+                System.out.printf("Rocket %d: Countdown: %d%n", n, i); // accessed from closure
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    System.out.printf("Countdown interrupted for rocket %d%n", n);
+                }
+            }
+            System.out.printf("Rocket %d: Liftoff!!!%n", n);
+        });
+    };
+
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            new Thread(new RocketLiftOff(i)).start();
+        var executor = Executors.newFixedThreadPool(4);
+        for (int k = 1; k <= 10; k++) {
+            rocketStart.accept(k, executor);
         }
     }
 }
