ebf0e3ae640c2196e83189b95bc2eb0d30f054c4
Trayan Iliev <office@iproduct.org>
2022-03-22 10:17:42 +0200

    word counting demo

--- 01-functions-intro/src/course/stream/streams/KeywordExtraction.java
+++ 01-functions-intro/src/course/stream/streams/KeywordExtraction.java
@@ -3,8 +3,26 @@ package course.stream.streams;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class KeywordExtraction {
+    public static final List<String> STOP_WORDS = List.of(
+            "when", "didn't", "wasn", "y", "few", "below", "into", "there", "his", "these", "about", "if", "again",
+            "too", "were", "then", "doing", "haven", "such", "this", "me", "the", "further", "whom", "having",
+            "mightn", "both", "him", "she", "don", "yourselves", "all", "do", "it's", "i", "what", "needn", "had",
+            "during", "weren", "wasn't", "up", "will", "just", "isn", "theirs", "own", "ll", "hadn", "aren't",
+            "while", "hasn't", "themselves", "re", "in", "she's", "wouldn't", "he", "couldn't", "their", "more",
+            "that'll", "ain", "t", "a", "was", "but", "couldn", "only", "been", "so", "until", "have", "from",
+            "those", "or", "our", "ourselves", "as", "doesn", "over", "how", "you'd", "where", "itself", "against",
+            "very", "you'll", "who", "needn't", "by", "no", "each", "its", "at", "we", "any", "ours", "mightn't",
+            "through", "does", "here", "didn", "between", "some", "which", "other", "did", "don't", "hasn", "won",
+            "not", "should", "haven't", "on", "now", "with", "you", "them", "himself", "under", "you've", "yours",
+            "they", "shouldn", "doesn't", "of", "can", "be", "above", "weren't", "wouldn", "for", "s", "and",
+            "hadn't", "herself", "nor", "should've", "am", "once", "ve", "an", "hers", "myself", "to", "her", "is",
+            "shan't", "are", "same", "being", "your", "than", "it", "aren", "shan", "d", "m", "isn't", "down",
+            "shouldn't", "you're", "why", "o", "yourself", "has", "my", "ma", "out", "that", "mustn't", "because",
+            "after", "before", "most", "off", "mustn", "won't");
+
     public static void main(String[] args) throws IOException {
         var path = Paths.get("data/fp_wiki.txt");
         Files.lines(path).skip(80).limit(5)
